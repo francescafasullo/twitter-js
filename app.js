765a3d775ad88355c17de059c6923d1d1cd0ca07
Francesca Fasullo <fjf2107@columbia.edu>
2017-04-25 12:47:40 -0400

    routing and views live test

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+
+const people = [{name: 'Francesca'}, {name: 'Julia'}, {name: 'Claire'}];
+
+app.use('/', routes);
 
 app.use('/', function (req, res, next) {
 	res.on('finish', function(){
@@ -8,14 +18,18 @@ app.use('/', function (req, res, next) {
   next();
 })
 
-app.get('/', function(req, res, next){
-    res.send('welcome!');
-    next();
-})
-
-app.use('/special/', function(req, res, next){
-	console.log('you\'ve reached the special area');
-})
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
 
 var server = app.listen(3000, () => {
     console.log("server listening");
