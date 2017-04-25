b86c946e3a3a89efde4e0ee870b5710639247471
Francesca Fasullo <fjf2107@columbia.edu>
2017-04-25 10:48:56 -0400

    finished configuring express

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.use('/', function (req, res, next) {
+	res.on('finish', function(){
+    console.log(req.method + " " + req.path + " " + res.statusCode);	
+	})
+  next();
+})
+
 app.get('/', function(req, res, next){
     res.send('welcome!');
     next();
 })
 
-app.use('/', function (req, res, next) {
-  if(res.status){
-  	 console.log(req.method + " " + req.path + " " + res.statusCode);
-  }
-  next();
+app.use('/special/', function(req, res, next){
+	console.log('you\'ve reached the special area');
 })
 
 var server = app.listen(3000, () => {
